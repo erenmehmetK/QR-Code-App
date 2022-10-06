8305034f68b03d409853df2070a96f22f615b878
Milan van der Burgh <mvanderburgh@student.scalda.nl>
2022-10-06 11:03:17 +0200

    - gitignore hopelijk gefixt

---
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Account.java
@@ -0,0 +1,54 @@
+package nl.delphinity.qr_goats.domain;
+
+import java.util.Objects;
+
+public class Account implements Comparable<Account> {
+
+	private int id;
+	private String wachtwoord;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getWachtwoord() {
+		return wachtwoord;
+	}
+
+	public void setWachtwoord(String wachtwoord) {
+		this.wachtwoord = wachtwoord;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, wachtwoord);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return id == other.id && Objects.equals(wachtwoord, other.wachtwoord);
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", wachtwoord=" + wachtwoord + "]";
+	}
+
+	@Override
+	public int compareTo(Account o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Student.java
@@ -0,0 +1,20 @@
+package nl.delphinity.qr_goats.domain;
+
+import java.util.Objects;
+
+public class Student extends Persoon {
+	
+	private int studentenNR;
+
+	
+
+
+	public int getStudentenNR() {
+		return studentenNR;
+	}
+
+	public void setStudentenNR(int studentenNR) {
+		this.studentenNR = studentenNR;
+	}	
+}
+
