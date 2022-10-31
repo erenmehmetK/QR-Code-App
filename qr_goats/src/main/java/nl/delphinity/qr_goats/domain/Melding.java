350471d97e5ed6ec59653a4dc6e2d5b8328b7537
Sahim <237010@student.scalda.nl>
2022-10-31 11:45:00 +0100

    no message

--- qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Melding.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Melding.java
@@ -1,5 +1,87 @@
 package nl.delphinity.qr_goats.domain;
 
-public class Melding {
+import java.util.Objects;
+
+public class Melding implements Comparable<Melding>{
+
+		private int id;
+		private String datum;
+		private TYPE type;
+		private String opmerking;
+		private Student student;
+		
+		enum TYPE{
+			LAAT, ZIEK
+	    }
+	
+	
+
+		public Melding(int id, String datum, TYPE type, String opmerking, Student student) {
+			
+		}
+		
+		@Override
+		public int compareTo(Melding other) {
+			return id - other.id;
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Melding other = (Melding) obj;
+			return Objects.equals(id, other.id);
+		}
+		
+		
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getDatum() {
+			return datum;
+		}
+
+		public void setDatum(String datum) {
+			this.datum = datum;
+		}
+
+		public TYPE getType() {
+			return type;
+		}
+
+		public void setType(TYPE type) {
+			this.type = type;
+		}
+
+		public String getOpmerking() {
+			return opmerking;
+		}
+
+		public void setOpmerking(String opmerking) {
+			this.opmerking = opmerking;
+		}
+
+		public Student getStudent() {
+			return student;
+		}
+
+		public void setStudent(Student student) {
+			this.student = student;
+		}
 
 }
