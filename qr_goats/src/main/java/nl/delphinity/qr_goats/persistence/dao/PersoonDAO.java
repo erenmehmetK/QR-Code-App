c9edd98319d2a1464552815887d880d2fc18d5a5
Jarno0-0 <jdeconinck2@student.scalda.nl>
2022-11-01 09:07:28 +0100

    no message

--- qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/dao/PersoonDAO.java
+++
@@ -1,8 +0,0 @@
-package nl.delphinity.qr_goats.persistence.dao;
-
-import nl.delphinity.qr_goats.domain.Persoon;
-import nl.delphinity.qr_goats.persistence.interfaces.IPersoonDAO;
-
-public class PersoonDAO extends GenericHibernateDAO<Persoon, String> implements IPersoonDAO {
-
-}
--- qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/interfaces/IPersoonDAO.java
+++
@@ -1,7 +0,0 @@
-package nl.delphinity.qr_goats.persistence.interfaces;
-
-import nl.delphinity.qr_goats.domain.Persoon;
-
-public interface IPersoonDAO extends IGenericDAO<Persoon, String> {
-	
-}
