5b29b1b7b424480bad1ce4f4c039f79798acef92
Milan van der Burgh <mvanderburgh@student.scalda.nl>
2022-11-16 09:58:32 +0100

    no message

---
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/factories/TestDAOfactory.java
@@ -0,0 +1,14 @@
+package nl.delphinity.qr_goats.persistence.factories;
+
+import nl.delphinity.qr_goats.persistence.interfaces.IAccountDAO;
+import nl.delphinity.qr_goats.domain.*;
+import nl.delphinity.qr_goats.persistence.dao.*;
+import nl.delphinity.qr_goats.persistence.interfaces.*;
+import nl.delphinity.qr_goats.persistence.utils.HibernateSessionManager;
+import org.hibernate.Session;
+
+public class TestDAOfactory extends DAOFactory {
+
+
+
+}
