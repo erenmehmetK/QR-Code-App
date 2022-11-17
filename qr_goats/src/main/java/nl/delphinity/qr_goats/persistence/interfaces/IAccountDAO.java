c0aef741f874fa3ce2dc244857cd12e017b325e6
Milan van der Burgh <mvanderburgh@student.scalda.nl>
2022-11-17 09:13:18 +0100

    dingen verwijderd die niet horen

--- qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/interfaces/IAccountDAO.java
+++
@@ -1,9 +0,0 @@
-package nl.delphinity.qr_goats.persistence.interfaces;
-
-import nl.delphinity.qr_goats.domain.Account;
-
-public interface IAccountDAO extends IGenericDAO<Account, Integer> {
-
-	public Account findbyemail(Account a);
-
-}
