e06622d696d37aa1c3308af35449bb71e69498fe
Jarno0-0 <jdeconinck2@student.scalda.nl>
2022-12-12 12:53:04 +0100

    no message

--- qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Account.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Account.java
@@ -1,10 +1,12 @@
 package nl.delphinity.qr_goats.domain;
 
+import java.util.Objects;
+
 import nl.delphinity.qr_goats.persistence.factories.DAOFactory;
 
 public class Account implements Comparable<Account> {
 
-	
+	private Integer id;
 	private String wachtwoord;
 	private String email;
 
@@ -38,8 +40,13 @@ public class Account implements Comparable<Account> {
 		}
 	}
 
+	public int getId() {
+		return id;
+	}
 
-
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getWachtwoord() {
 		return wachtwoord;
@@ -50,6 +57,12 @@ public class Account implements Comparable<Account> {
 	}
 
 	
+	//hasht de id zodat het object vergelijkbaar is
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
 	// returnt een boolean gebaseerd op als het object gelijk is
 	@Override
 	public boolean equals(Object obj) {
@@ -60,29 +73,39 @@ public class Account implements Comparable<Account> {
 		if (getClass() != obj.getClass())
 			return false;
 		Account other = (Account) obj;
-	return true;
+		return id == other.id;
 	}
 
 	// returnt waardes van account variabelen als een string
 	@Override
 	public String toString() {
-		return "wachtwoord=" + wachtwoord + "]";
+		return "Account [id=" + id + ", wachtwoord=" + wachtwoord + "]";
 	}
 
 	// returnt een nummer gebaseerd op als het object gelijk is, gebruikt voor
 	// treesets sorteren
+	@Override
+
+	public int compareTo(Account other) {
+
+		int temp = id.compareTo(other.id);
+		if (temp == 0) {
+
+			int temp2 = wachtwoord.compareTo(wachtwoord);
+			if (temp2 == 0) {
+
+				return temp2;
+			}
+		}
+		return temp;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	@Override
-	public int compareTo(Account o) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 }
