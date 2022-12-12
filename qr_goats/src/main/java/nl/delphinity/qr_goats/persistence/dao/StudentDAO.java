4469cc444cebc50b625e2638a530b6106b47163d
Roi Eielts <reielts@student.scalda.nl>
2022-12-12 13:03:52 +0100

    tussen commit

--- qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/dao/StudentDAO.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/dao/StudentDAO.java
@@ -1,8 +1,17 @@
 package nl.delphinity.qr_goats.persistence.dao;
 
+import nl.delphinity.qr_goats.domain.Account;
 import nl.delphinity.qr_goats.domain.Student;
 import nl.delphinity.qr_goats.persistence.interfaces.IStudentDAO;
 
 public class StudentDAO extends GenericHibernateDAO<Student, String> implements IStudentDAO{
+	
+	@Override
+	public Student findByEmail(Account a) {
+		getSession().beginTransaction();
+		Student entity = getSession().find(getPersistentClass(), a.getEmail());
+		getSession().getTransaction().commit();
+		return entity;
+	}
 
 }
