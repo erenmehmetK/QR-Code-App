20721f4ce2226eae639a2d93526d47db2a4245ec
Jarno0-0 <jdeconinck2@student.scalda.nl>
2022-12-12 12:54:57 +0100

    no message

--- qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Account.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Account.java
@@ -6,7 +6,7 @@ import nl.delphinity.qr_goats.persistence.factories.DAOFactory;
 
 public class Account implements Comparable<Account> {
 
-	private Integer id;
+
 	private String wachtwoord;
 	private String email;
 
@@ -40,13 +40,7 @@ public class Account implements Comparable<Account> {
 		}
 	}
 
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getWachtwoord() {
 		return wachtwoord;
@@ -56,49 +50,14 @@ public class Account implements Comparable<Account> {
 		this.wachtwoord = wachtwoord;
 	}
 
-	
-	//hasht de id zodat het object vergelijkbaar is
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	// returnt een boolean gebaseerd op als het object gelijk is
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Account other = (Account) obj;
-		return id == other.id;
-	}
-
 	// returnt waardes van account variabelen als een string
 	@Override
 	public String toString() {
-		return "Account [id=" + id + ", wachtwoord=" + wachtwoord + "]";
+		return "Account [wachtwoord=" + wachtwoord + "]";
 	}
 
 	// returnt een nummer gebaseerd op als het object gelijk is, gebruikt voor
 	// treesets sorteren
-	@Override
-
-	public int compareTo(Account other) {
-
-		int temp = id.compareTo(other.id);
-		if (temp == 0) {
-
-			int temp2 = wachtwoord.compareTo(wachtwoord);
-			if (temp2 == 0) {
-
-				return temp2;
-			}
-		}
-		return temp;
-	}
 
 	public String getEmail() {
 		return email;
