b75310aad8aeb8e22ba95c289e39de841836d546
Eren Kaya <ekaya@student.scalda.nl>
2022-12-13 11:40:56 +0100

    LDM Melding, laatMelding en ziekmelding

--- qr_goats/src/main/java/nl/delphinity/qr_goats/domain/LaatMelding.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/LaatMelding.java
@@ -1,7 +1,12 @@
 package nl.delphinity.qr_goats.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class LaatMelding extends Melding {
 	
+	@Column(name = "Opmerking", nullable = true)
 	private String opmerking;
 
 	public String getOpmerking() {
--- qr_goats/src/main/java/nl/delphinity/qr_goats/domain/ZiekMelding.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/ZiekMelding.java
@@ -1,5 +1,8 @@
 package nl.delphinity.qr_goats.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class ZiekMelding extends Melding {
 
 }
