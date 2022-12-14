3a02f05937442a638ef6ac02b5da24027cc31288
Roi Eielts <reielts@student.scalda.nl>
2022-12-14 11:23:34 +0100

    i forgor

--- qr_goats/src/main/java/nl/delphinity/qr_goats/controller/interceptors/LoginInterceptor.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/controller/interceptors/LoginInterceptor.java
@@ -3,16 +3,11 @@ package nl.delphinity.qr_goats.controller.interceptors;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
-import nl.delphinity.qr_goats.domain.Account;
 import nl.delphinity.qr_goats.persistence.factories.DAOFactories;
 import nl.delphinity.qr_goats.persistence.factories.DAOFactory;
 
 public class LoginInterceptor extends AbstractInterceptor {
 
-	private Account a;
-	private Account a2;
-	private Account a3;
-	
 	@Override
 	public void init() {
 		 
