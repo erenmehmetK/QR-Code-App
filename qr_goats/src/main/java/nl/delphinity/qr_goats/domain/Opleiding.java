3817f22def61b98838db3a2a58bd75f074f74848
Eren Kaya <ekaya@student.scalda.nl>
2023-01-10 12:41:22 +0100

    ldm

--- qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Opleiding.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/domain/Opleiding.java
@@ -1,10 +1,13 @@
 package nl.delphinity.qr_goats.domain;
 
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -19,17 +22,27 @@ public class Opleiding {
 	
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "opleidingID", nullable = false, unique = true)
 	private int id;
 	
+	@Column(name = "opleiding_naam", length = 20, nullable = false, unique = false)
+	private String naam;
+	
 	@OneToMany(mappedBy = "opleiding", cascade = CascadeType.ALL)
-	private TreeSet<Student> studenten = new TreeSet<Student>();
+	private SortedSet<Student> studenten;
 	
 	public Opleiding() {
 
 	}
 
 	public Student findStudent(String studentenNR) {
+		
+		if (studenten == null) {
+
+			studenten = new TreeSet<Student>();
+
+		}
 
 		for (Student s : studenten) {
 			if (s.getStudentenNR().equals(studentenNR)) {
@@ -39,39 +52,76 @@ public class Opleiding {
 		return null;
 	}
 
-	public void studentZiekMelden(String studentenNR) {
+	public Melding studentZiekMelden(String studentenNR) {
+		
+		if (studenten == null) {
+
+			studenten = new TreeSet<Student>();
+
+		}
+		
+		Melding m1 = null;
 		
 		for (Student s : studenten) {
 			if (s.getStudentenNR().equals(studentenNR)) {
-				s.ziekMelden();
+				m1 = s.ziekMelden();
 				System.out.println(s.getMeldingen());
 
 				
 			}
 		}
-
+       return m1;
 	}
 
-	public void studentLaatMelden(String studentenNR, String opmerking, String reden) {
+	public Melding studentLaatMelden(String studentenNR, String opmerking, String reden) {
+		
+		
+		
+		if (studenten == null) {
+
+			studenten = new TreeSet<Student>();
+
+		}
+		
+		Melding m1 = null;
 		
 		for (Student s : studenten) {
 			if (s.getStudentenNR().equals(studentenNR)) {
-				s.laatMelden(opmerking, reden);
+				m1 = s.laatMelden(opmerking, reden);
 				System.out.println(s.getMeldingen());
 				
 			}
+			
 		}
+		return m1;
 	}
 
 	// TODO remove after testing
 	public void loadTestData() {
 		// Call naar DAO van Opleiding en die returned een hoof studenten voor je
 		// treeset
-		studenten = (TreeSet<Student>) DAOFactory.getTheFactory().getStudentDAO().findAll();
+		studenten = (SortedSet<Student>) DAOFactory.getTheFactory().getStudentDAO().findAll();
 
 	}
+	
+	public void addStudent(Student s) {
+		
+		if (studenten == null) {
 
-	public TreeSet<Student> getStudenten() {
+			studenten = new TreeSet<Student>();
+
+		}
+		
+		studenten.add(s);
+		s.setOpleiding(this);
+	}
+	
+	public void removeStudent(Student s) {
+		studenten.remove(s);
+		s.setOpleiding(null);
+	}
+
+	public SortedSet<Student> getStudenten() {
 		return studenten;
 	}
 	
@@ -79,7 +129,7 @@ public class Opleiding {
 		return id + " " + studenten;
 	}
 
-	public void setStudenten(TreeSet<Student> studenten) {
+	public void setStudenten(SortedSet<Student> studenten) {
 		this.studenten = studenten;
 	}
 	
@@ -90,6 +140,14 @@ public class Opleiding {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
 	}
 
 }
