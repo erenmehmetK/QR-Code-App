bbcdb96d37cacd89045c043cfa480c99364edb4d
Eren Kaya <ekaya@student.scalda.nl>
2023-01-11 09:57:13 +0100

    ok

--- qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/dao/MeldingDAO.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/dao/MeldingDAO.java
@@ -1,12 +1,18 @@
 package nl.delphinity.qr_goats.persistence.dao;
 
 import nl.delphinity.qr_goats.domain.Melding;
+import nl.delphinity.qr_goats.domain.Student;
 import nl.delphinity.qr_goats.persistence.interfaces.IMeldingDAO;
 
 
 public class MeldingDAO extends GenericHibernateDAO<Melding, Integer> implements IMeldingDAO{
 
-
+	  
+		@Override
+		public Melding findByStudent(Student s) {
+			Melding entity = getSession().find(getPersistentClass(), s.getId());
+			return entity;
+		}
 	
 	
 }
--- qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/interfaces/IMeldingDAO.java
+++ qr_goats/src/main/java/nl/delphinity/qr_goats/persistence/interfaces/IMeldingDAO.java
@@ -1,7 +1,10 @@
 package nl.delphinity.qr_goats.persistence.interfaces;
 
 import nl.delphinity.qr_goats.domain.Melding;
+import nl.delphinity.qr_goats.domain.Student;
 
 public interface IMeldingDAO extends IGenericDAO<Melding, Integer>{
+	
+	public abstract Melding findByStudent(Student s);
 
 }
