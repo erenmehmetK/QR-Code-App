a4cd77cdc99a07fc04064f5f388e8f2fa288f2cf
MiguelBrugge <103996305+MiguelBrugge@users.noreply.github.com>
2023-01-12 09:41:40 +0100

    OpleidingTest uit comments gehaald.

--- qr_goats/src/test/java/qr_goats/OpleidingTest.java
+++ qr_goats/src/test/java/qr_goats/OpleidingTest.java
@@ -1,64 +1,64 @@
-//package qr_goats;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//
-//import nl.delphinity.qr_goats.domain.Opleiding;
-//import nl.delphinity.qr_goats.domain.OpleidingFacade;
-//import nl.delphinity.qr_goats.domain.Student;
-//
-//
-//public class OpleidingTest {
-//	
-//	
-//	private Opleiding opleiding;
-//
-//	@Before
-//	public void setUp() {
-//		opleiding = OpleidingFacade.getInstance().getOpleiding();
-//		opleiding.loadTestData();
-//	}
-//	
-//	@Test
-//	public void OpleidingNullTest() {
-//		//Checkt of de opleiding niet null is
-//		assertNotNull(opleiding);
-//	}
-//	
-//	@Test
-//	public void OpleidingStudentTest() {
-//		
-//        //Checkt of de treeset studenten heeft studenten
-//		assertTrue(opleiding.getStudenten().size() == 100);
-//	}
-//	
-//	
-//	@Test
-//	public void OpleidingStudentMeldingTest() {
-//		
-//		opleiding.studentLaatMelden("100000", "Mijn kat moest naar de ziekenhuis", "Overige");
-//        opleiding.studentZiekMelden("100001");
-//		
-//        
-//        //for each loop om te checken of de student meldt laat.
-//		for(Student s: opleiding.getStudenten()) {
-//			if(s.getStudentenNR().equals("100000")) {
-//				
-//				assertTrue(s.getMeldingen().size() == 1);
-//			}
-//		}
-//		
-//		//for each loop om te checken of de student meldt ziek.
-//		for(Student s: opleiding.getStudenten()) {
-//			if(s.getStudentenNR().equals("100001")) {
-//				
-//				assertTrue(s.getMeldingen().size() == 1);
-//			}
-//		}
-//		
-//	}
-//	
-//}
+package qr_goats;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import nl.delphinity.qr_goats.domain.Opleiding;
+import nl.delphinity.qr_goats.domain.OpleidingFacade;
+import nl.delphinity.qr_goats.domain.Student;
+
+
+public class OpleidingTest {
+	
+	
+	private Opleiding opleiding;
+
+	@Before
+	public void setUp() {
+		opleiding = OpleidingFacade.getInstance().getOpleiding();
+		opleiding.loadTestData();
+	}
+	
+	@Test
+	public void OpleidingNullTest() {
+		//Checkt of de opleiding niet null is
+		assertNotNull(opleiding);
+	}
+	
+	@Test
+	public void OpleidingStudentTest() {
+		
+        //Checkt of de treeset studenten heeft studenten
+		assertTrue(opleiding.getStudenten().size() == 100);
+	}
+	
+	
+	@Test
+	public void OpleidingStudentMeldingTest() {
+		
+		opleiding.studentLaatMelden("100000", "Mijn kat moest naar de ziekenhuis", "Overige");
+        opleiding.studentZiekMelden("100001");
+		
+        
+        //for each loop om te checken of de student meldt laat.
+		for(Student s: opleiding.getStudenten()) {
+			if(s.getStudentenNR().equals("100000")) {
+				
+				assertTrue(s.getMeldingen().size() == 1);
+			}
+		}
+		
+		//for each loop om te checken of de student meldt ziek.
+		for(Student s: opleiding.getStudenten()) {
+			if(s.getStudentenNR().equals("100001")) {
+				
+				assertTrue(s.getMeldingen().size() == 1);
+			}
+		}
+		
+	}
+	
+}
